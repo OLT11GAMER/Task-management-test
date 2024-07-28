342e56b95fc6a3e6ad0df217273f68a077ac2cdd
OLT11GAMER <93716411+OLT11GAMER@users.noreply.github.com>
2024-07-28 12:11:11 +0200

    d

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,35 @@
+data "google_container_engine_versions" "default"{
+  location = "europe-central2-a"
+}
+data "google_client_config" "current"{
+}
+
+resource "google_container_cluster" "default" {
+  name               = "my-gke-cluster"
+  location           = "eu-central2-a"
+  remove_default_node_pool = true
+  initial_node_count = 1
+  min_master_version = data.google_container_engine_versions.default.latest_master_version
+  network            = "default"
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 32
+    # oauth_scopes = [
+    #   "https://www.googleapis.com/auth/logging.write",
+    #   "https://www.googleapis.com/auth/monitoring.write",
+    #   "https://www.googleapis.com/auth/devstorage.read_only",
+    #   "https://www.googleapis.com/auth/servicecontrol",
+    #   "https://www.googleapis.com/auth/service.management.readonly",
+    #   "https://www.googleapis.com/auth/trace.append",
+    # ]
+
+  }
+  provisioner "local-exec"{
+    when = destroy
+    command = "sleep 90"
+  }
+}
 resource "google_compute_instance" "default" {
   name         = "terraform-vm"
   machine_type = "e2-medium"
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -26,3 +26,12 @@ provider "google" {
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
+provider "kubernetes"{
+  host = google_container_cluster.default.endpoint
+  token = data.google_client_config.current.access_token
+  client_certifiate = base64code(google_container_cluster.default.master_auth[0].client_certificate)
+  cleint_key = base64code(google_container_cluster.default.master_auth[0].client_key)
+  cluster_ca_certificate = base64code(
+    base64code(google_container_cluster.default.master_auth[0].cluster_ca_certificate)
+  )
+}
